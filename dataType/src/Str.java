d6d4f31044802d451ad3de844ef1aac66e2424ba
ParkSanggi <roqkfwkehlwk@naver.com>
2019-11-23 20:24:54 +0900

    add dataType

---
+++ dataType/src/Str.java
@@ -0,0 +1,14 @@
+
+public class Str {
+
+	public static void main(String[] args) {
+		System.out.println("String");
+		// 자바에서 작은따옴표는 캐릭터를 의미
+		// 큰따옴표는 문자열
+		System.out.println('c');
+		
+		System.out.println("Hello\nWorld");
+		System.out.println("Hello \"World\"");
+	}
+
+}
---
+++ dataType/src/Variable.java
@@ -0,0 +1,15 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int a  = 1;
+		double b = 2.1;
+		String c = "Hello World";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
